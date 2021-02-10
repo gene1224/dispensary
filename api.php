7dde2da869c273bfd33d925965049a673af800aa
Gene Sescon <sescongene.dev@gmail.com>
2021-02-10 18:50:09 +0800

    Ajax inventory management

--- api.php
+++ api.php
@@ -1,5 +1,29 @@
-<?php 
+<?php
 
-function get_stocks_status_API() {
+function ajax($url, $data = [], $method = 'GET')
+{
 
+    $final_url = $url;
+
+    $ch = curl_init();
+    if ($method == 'POST') {
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    } else {
+        $final_url = $url . '?' . http_build_query($data);
+    }
+
+    curl_setopt($ch, CURLOPT_URL, $final_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+
+    curl_close($ch);
+
+    $result = json_decode($response, true);
+
+    if (json_last_error() === JSON_ERROR_NONE) {
+        return $result;
+    } else {
+        return $result;
+    }
 }
--- product.php
+++ product.php
@@ -1,14 +1,20 @@
 <?php
-// define the woocommerce_product_is_in_stock callback
+
+require 'api.php';
+
 function filter_woocommerce_product_is_in_stock($instock_this_get_stock_status, $instance)
 {
     $product_id = $instance->get_id();
 
     $source_site_id = get_post_meta($product_id, 'source_site_id', true);
 
+    $source_site_url = get_post_meta($product_id, 'source_site_url', true);
+
     $source_product_id = get_post_meta($product_id, 'source_product_id', true);
 
     if ($source_site_id && $source_product_id) {
+        return get_stocks_status($source_product_id, 0, $source_site_url);
+    } else if ($source_site_id && $source_product_id) {
         return get_stocks_status($source_product_id, $source_site_id);
     } else {
         return $instock_this_get_stock_status;
@@ -16,9 +22,20 @@ function filter_woocommerce_product_is_in_stock($instock_this_get_stock_status, 
 
 };
 
-
 function get_stocks_status($source_product_id, $source_site_id, $site_url = '')
 {
+
+    if ($site_url != '') {
+        $result = ajax(
+            'http://source.wpms.net/wp-admin/admin-ajax.php',
+            array(
+                'action' => 'get_stocks_status_ajax',
+                'product_id' => $source_product_id,
+            )
+        );
+        return $result['in_stock'];
+    }
+
     switch_to_blog($source_site_id);
 
     $product = wc_get_product($source_product_id);
@@ -31,7 +48,6 @@ function get_stocks_status($source_product_id, $source_site_id, $site_url = '')
 
     return $stock_status;
 }
-
 
 function get_stocks_status_ajax()
 {
@@ -53,7 +69,6 @@ function get_stocks_status_ajax()
 
     die();
 }
-
 
 add_action("wp_ajax_get_stocks_status_ajax", "get_stocks_status_ajax");
 add_action("wp_ajax_nopriv_get_stocks_status_ajax", "get_stocks_status_ajax");
