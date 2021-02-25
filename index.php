ebf1603152fb3240d6ef0e0c30c5f2136287a512
Gene Sescon <sescongene.dev@gmail.com>
2021-02-25 20:15:20 +0800

    Start of woocomerce import

--- index.php
+++ index.php
@@ -11,9 +11,7 @@ Author: Gene Sescon
 Author URI: https://example.com/
 License: GPLv2 or later
 Text Domain: QRXDispensary
-*/
-
-
+ */
 
 require_once 'vendor/autoload.php';
 
@@ -24,3 +22,15 @@ require 'product.php';
 require 'multisite.php';
 
 require 'admin/index.php';
+
+function copy_products_function()
+{
+    $array_of_product = array(
+        array(
+            'product_id' => '',
+            'listing_price' => '',
+        ),
+    );
+    die();
+}
+add_action('wp_ajax_copy_products', 'copy_products_function');
