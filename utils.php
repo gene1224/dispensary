d54688ec5c0fadebcd37182cecd77afb224b763d
Gene Sescon <sescongene@gmail.com>
2021-04-06 06:30:25 +0000

    Fixed sql

--- utils.php
+++ utils.php
@@ -293,8 +293,6 @@ function calculate_visitor_total($site_id)
     $website_visitors_total = 0;
 
     $table_visitors = $wpdb->base_prefix . $site_id . '_statistics_visitor';
-    
-    echo $table_visitors;
 
     $result_visitors = $wpdb->get_results("SELECT * FROM $table_visitors", OBJECT);
 
@@ -310,8 +308,8 @@ function get_page_view_count($site_id)
 
     $page_views_table = $table_visitors = $wpdb->base_prefix . $site_id . '_statistics_pages';
 
-    $page_views_sql = "SELECT cast(`last_counter` as date) as date_visited, count(ID) as count FROM `" . $page_views_table . "` GROUP BY cast(`last_counter` as date) ORDER BY `date_visited` DESC LIMIT 7";
-
+    $page_views_sql = "SELECT cast(`date` as date) as date_visited, SUM(`count`) as count FROM `". $page_views_table ."` GROUP BY cast(`date` as date) ORDER BY `date_visited` DESC LIMIT 7";
+    echo $page_views_sql;
     return $wpdb->get_results($page_views_sql, ARRAY_A);
 }
 
@@ -321,7 +319,7 @@ function get_visitor_counts($site_id)
     
     $visitors_table = $table_visitors = $wpdb->base_prefix . $site_id . '_statistics_visitor';
 
-    $visitors_sql = "SELECT cast(`date` as date) as date_visited, SUM(`count`) as count FROM `" . $visitors_table . "` GROUP BY cast(`date` as date) ORDER BY `date_visited` DESC LIMIT 7";
-
+    $visitors_sql = "SELECT cast(`last_counter` as date) as date_visited, COUNT(ID) as count FROM `". $visitors_table ."` GROUP BY cast(`last_counter` as date) ORDER BY `date_visited` DESC LIMIT 7";
+    
     return $wpdb->get_results($visitors_sql, ARRAY_A);
 }
